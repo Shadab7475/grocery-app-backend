30ed6615bd63bc42cf5af6e9d26d0287d889604a
Shadab7475 <shadab4542529@gmail.com>
2026-04-07 16:17:23 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,23 +1,3 @@
-// import express from "express"
-// const router = express.Router(); 
-
-
-// import upload from "../middleware/upload.js"; 
-
-// import { addProduct, getProducts } from "../controllers/productController.js"
-
-
-// router.post("/add", upload.single("image"), addProduct);
-
-
-// router.get("/", getProducts);
-
-// export default router
-
-
-// export default routerfjfjffj
-
-
 
 import express from "express";
 const router = express.Router();
